87c95a0de94c01533dc3419a1a999846e407138e
Johan Tibell <johan.tibell@gmail.com>
2024-10-25 12:16:49 +0200

    Fix eslint errors and warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,8 +8,20 @@ export default [
   {files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']},
   {languageOptions: {globals: globals.browser}},
   {plugins: {simpleImportSort: simpleImportSort}},
-  {ignores: ['node_modules/', 'src/models/', 'src/services/', 'dist/']},
+  {ignores: ['node_modules/', 'dist/']},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 ];
